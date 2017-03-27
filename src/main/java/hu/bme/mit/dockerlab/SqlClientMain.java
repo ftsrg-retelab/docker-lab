ee05e5cb53c5387112611190d2dcae1dd133c355
Gabor Szarnyas <szarnyasg@gmail.com>
2017-03-27 14:13:07 +0200

    Fixes

--- src/main/java/hu/bme/mit/dockerlab/SqlClientMain.java
+++ src/main/java/hu/bme/mit/dockerlab/SqlClientMain.java
@@ -12,13 +12,13 @@ public class SqlClientMain {
 	private static final String PASSWORD = "retelab";
 
 	public static void main(String[] args) throws SQLException {
-		final String url = "jdbc:mysql://localhost:3306/golf?allowMultiQueries=true&useSSL=false";
+		final String url = "jdbc:mysql://172.17.0.2:3306/golf?useSSL=false";
 
 		try (final Connection connection = DriverManager.getConnection(url, USER, PASSWORD)) {
-			try (final Statement statement = connection.prepareStatement("SELECT * FROM golf LIMIT 10")) {
-				try (ResultSet rs = statement.executeQuery(url)) {
+			try (final Statement statement = connection.createStatement()) {
+				try (ResultSet rs = statement.executeQuery("SELECT * FROM golf")) {
 					while (rs.next()) {
-						System.out.println(rs);
+						System.out.println(rs.getString(1));
 					}
 				}
 			}
